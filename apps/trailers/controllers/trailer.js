ad94e2f021a0ded3bb64b301b529067c87202743
Joshua Holt <holt.josh@gmail.com>
2010-05-19 02:59:20 -0400

    removed unneeded code in the trailer object controller

--- apps/trailers/controllers/trailer.js
+++ apps/trailers/controllers/trailer.js
@@ -14,33 +14,8 @@
 */
 Trailers.trailerController = SC.ObjectController.create(
 /** @scope Trailers.trailerController.prototype */ {
-
-  contentBinding: 'Trailers.trailersController.selection',
   
-  trailerViewContent: function() {
-    var trailerView = SC.View.create();
-    var preview = this.get('preview');
-    if (this.get('hasContent')) {
-      trailerView = SC.VideoView.create({
-        value: preview,
-        mediaControl: 'none',
-        didCreateLayer: function() {
-          sc_super();
-          var vo = this.$('video');                
-          vo.attr('controls', 'controls');
-          this.set('layerLocationNeedsUpdate',YES);
-          this.updateLayerIfNeeded();
-        }
-      });
-    }
-    return trailerView;
-  }.property().cacheable(),
+  contentBinding: 'Trailers.trailersController.selection'
   
-  playTrailer: function() {
-    var trailer = this.get('trailerViewContent');
-    if (trailer) {
-      trailer.play(); 
-    }
-  }
 
 });
