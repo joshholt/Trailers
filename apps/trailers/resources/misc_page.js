0c81893a38d3e9bf910ad7d082509e29ecadbe32
Joshua Holt <holt.josh@gmail.com>
2010-05-19 03:01:46 -0400

    Added a miscPage to hold the Misc Crap

---
+++ apps/trailers/resources/misc_page.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   Trailers - miscPage
+// Copyright: ©2010 Metal Rooster Design.
+// ==========================================================================
+/*globals Trailers */
+
+Trailers.miscPage = SC.Page.design({
+  
+  loaderView: SC.ProgressView.design({
+    layout: {centerX: 0, centerY: 0, width: 250, height: 24 }
+  }),
+  
+  failureMessageView: SC.LabelView.design({
+    layout: { centerX: 0, centerY: 0, width: 300, height: 24 },
+    value: 'There was a problem loading the trailers list....',
+    fontWeight: SC.BOLD_WEIGHT
+  })
+  
+});
