eb735578bf36d3b71d8900d0bc6d556b86d251ad
Joshua Holt <holt.josh@gmail.com>
2010-05-19 03:02:12 -0400

    Added a simpleVideoview (HTML5 only)

---
+++ apps/trailers/views/simple_video.js
@@ -0,0 +1,29 @@
+// ==========================================================================
+// Project:   Trailers.SimpleVideoView
+// Copyright: ©2010 Metal Rooster Design.
+// ==========================================================================
+/*globals Trailers */
+
+/** @class
+
+  This soul purpose of this view is to hack around the fact the you can't 
+  just swap out the value (video URL) to make the video change in a 
+  SC.VideoView
+
+  @extends SC.View
+*/
+Trailers.SimpleVideoView = SC.View.extend(
+/** @scope Trailers.VideoHackView.prototype */ {
+  
+  videoURL: null,
+  
+  displayProperties: ['videoURL'],
+  
+  render: function(context, firstTime) {
+    var url = this.get('videoURL');
+    var videoStyle = {'position':'absolute', 'top':'0px', 'width':'640px', 'bottom':'30px'};
+    context.begin('video').css(videoStyle).attr({'src': url, 'controls':'true'}).end();
+    sc_super();
+  }
+
+});
